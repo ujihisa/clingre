86e6acc30da109ed7286f9bb8b0b8ab0605c7d0b
ujihisa <ujihisa@gmail.com>
2013-06-11 21:17:35 -0700

    project

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clingre "0.1.0-SNAPSHOT"
+(defproject clingre "1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :license {:name "GPL-3+"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
