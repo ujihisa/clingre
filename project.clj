623fe3956276c3c4b199386dfe4f2506694ad50c
ujihisa <ujihisa@gmail.com>
2013-06-12 00:17:55 -0700

    comet worked!

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "FIXME: write description"
   :url "https://github.com/ujihisa/clingre"
   :license {:name "GPL-3+"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.2"]
+                 [org.clojure/data.json "0.2.2"]]
+  :main clingre.core)
--- src/clingre/core.clj
+++ src/clingre/core.clj
@@ -1,6 +1,74 @@
-(ns clingre.core)
+(ns clingre.core
+  (:require clj-http.client)
+  (:require clojure.data.json))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def clingre-key "oWBgRP")
+
+(defn get-session []
+  (let [json (clj-http.client/post
+               "http://lingr.com/api/session/create"
+               {:form-params
+                {:user "ujihisa@gmail.com"
+                 :password ""
+                 :app_key clingre-key}})
+        body (try
+               (clojure.data.json/read-str (:body json))
+               (catch java.io.EOFException e {}))]
+    (when (= (body "status") "ok")
+      (body "session"))
+    #_(when (= (-> json :body :status) "ok")
+      (prn 'cool))))
+
+(defn get-rooms [session]
+  (let [json (clj-http.client/get
+               "http://lingr.com/api/user/get_rooms"
+               {:query-params
+                {:session session
+                 :app_key clingre-key}})
+        body (try
+               (clojure.data.json/read-str (:body json))
+               (catch java.io.EOFException e {}))]
+    (when (= (body "status") "ok")
+      (body "rooms"))
+    #_(when (= (-> json :body :status) "ok")
+      (prn 'cool))))
+
+(defn subscribe-rooms [session rooms]
+  (let [json (clj-http.client/post
+               "http://lingr.com/api/room/subscribe"
+               {:form-params
+                {:session session
+                 :rooms rooms
+                 :app_key clingre-key}})
+        body (try
+               (clojure.data.json/read-str (:body json))
+               (catch java.io.EOFException e {}))]
+    (when (= (body "status") "ok")
+      (body "counter"))))
+
+(defn observe [session counter]
+  (let [json (clj-http.client/get
+                       "http://lingr.com/api/event/observe"
+                       {:query-params
+                        {:session session
+                         :counter counter
+                         :app_key clingre-key}})
+                body (try
+                       (clojure.data.json/read-str (:body json))
+                       (catch java.io.EOFException e {}))]
+    (when (= (body "status") "ok")
+      body)
+    #_(when (= (-> json :body :status) "ok")
+      (prn 'cool))))
+
+(defn -main [& args]
+  (case args
+    ["-h"] (prn 'help)
+    (when-let [session #_"KZwqNz" (get-session)]
+      (when-let [rooms (get-rooms session)]
+        (prn 'session session 'rooms rooms)
+        (loop [counter (subscribe-rooms session ["computer_science" "vim"])]
+          (let [result (observe session counter)]
+            (if-let [new-counter (result "counter")]
+              (do (prn result) (recur new-counter))
+              (recur counter))))))))
