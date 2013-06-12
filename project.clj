57e22c760fcb85678ffc80f30f728564f23122e3
ujihisa <ujihisa@gmail.com>
2013-06-11 21:17:48 -0700

    url

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clingre "1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ujihisa/clingre"
   :license {:name "GPL-3+"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
