73ff671babd85dcd30fab0da79704e68a2766572
ujihisa <ujihisa@gmail.com>
2013-06-11 21:15:11 -0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clingre
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clingre/core_test.clj
@@ -0,0 +1,7 @@
+(ns clingre.core-test
+  (:use clojure.test
+        clingre.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
